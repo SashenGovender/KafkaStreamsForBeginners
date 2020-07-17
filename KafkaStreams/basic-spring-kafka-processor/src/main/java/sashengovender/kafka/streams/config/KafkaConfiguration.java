b3002670f9cccfbc7e2ff96d90ca4acf6b5429f2
Sashen <sashen50@gmail.com>
2020-07-17 20:21:15 +0200

    renamed spring boot project

---
+++ KafkaStreams/basic-spring-kafka-processor/src/main/java/sashengovender/kafka/streams/config/KafkaConfiguration.java
@@ -0,0 +1,28 @@
+package sashengovender.kafka.streams.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.kafka")
+public class KafkaConfiguration {
+
+  private String bootstrapServers;
+  private KafkaStreamsConfig streams;
+
+  public String getBootstrapServers() {
+    return bootstrapServers;
+  }
+
+  public void setBootstrapServers(String bootstrapServers) {
+    this.bootstrapServers = bootstrapServers;
+  }
+
+  public KafkaStreamsConfig getStreams() {
+    return streams;
+  }
+
+  public void setStreams(KafkaStreamsConfig streams) {
+    this.streams = streams;
+  }
+}
